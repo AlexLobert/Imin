08fc9d18bf3113a735818e2e866519adf0868b0f
Alex Lobert <alex.t.lobert@gmail.com>
2026-01-21 20:02:30 -0600

    Initial Commit

---
+++ Iminapp/IminappApp.swift
@@ -0,0 +1,17 @@
+//
+//  IminappApp.swift
+//  Iminapp
+//
+//  Created by Alex Lobert on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct IminappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IminappUITests/IminappUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  IminappUITestsLaunchTests.swift
+//  IminappUITests
+//
+//  Created by Alex Lobert on 1/21/26.
+//
+
+import XCTest
+
+final class IminappUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
